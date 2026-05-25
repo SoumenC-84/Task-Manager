479b78861cf9d65ce23a19c7f352b2babfc94f73
SoumenC-84 <soumen.chakraborty29@gmail.com>
2026-05-25 12:56:13 +0530

    initial Commit

---
+++ gateway/ApiGateway/Program.cs
@@ -0,0 +1,25 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json");
+
+
+builder.Services.AddOcelot();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+var app = builder.Build();
+app.UseCors("AllowFrontend");
+await app.UseOcelot();
+
+app.Run();
