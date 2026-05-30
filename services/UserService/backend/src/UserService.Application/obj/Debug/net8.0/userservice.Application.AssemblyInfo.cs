41c3ab3c62d358905635614664fcc43faa935c31
SoumenC-84 <soumen.chakraborty29@gmail.com>
2026-05-30 22:15:05 +0530

    Azure Connection string added

--- services/UserService/backend/src/UserService.Application/obj/Debug/net8.0/userservice.Application.AssemblyInfo.cs
+++ services/UserService/backend/src/UserService.Application/obj/Debug/net8.0/userservice.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de88f93b4b98ec9e26823fc3a802d536abf9e5e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440c122f7ce5f777f711e05905d4bab13e4fee10")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
